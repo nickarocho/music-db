9678b8fdbe686dbf923a2441ef3fdf3000332c49
Shynar Galiyeva <shynar@blacklev.com>
2018-02-05 14:21:59 -0800

    add create, partials, matesrialize

--- routes/index.js
+++ routes/index.js
@@ -16,7 +16,7 @@ router.get('/auth/facebook', passport.authenticate(
 router.get('/auth/facebook/callback', passport.authenticate(
   'facebook',
   {
-    successRedirect : '/',
+    successRedirect : '/profile',
     failureRedirect : '/'
   }
 ));
--- server.js
+++ server.js
@@ -12,6 +12,7 @@ require('dotenv').config();
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+// var credits = require('./routes/credits');
 
 var app = express();
 
@@ -38,7 +39,8 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 app.use('/', index);
-app.use('/users', users);
+app.use('/', users);
+// app.use('/')
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
