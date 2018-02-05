e714bded81fa94a71959e51338db3d112fc25010
Kaylee Buscemi <kaylee.buscemi@gmail.com>
2018-02-05 11:31:13 -0800

    fb oauth

--- config/passport.js
+++ config/passport.js
@@ -1,9 +1,40 @@
 var passport = require('passport');
 var FacebookStrategy = require('passport-facebook').Strategy;
+var User = require('../models/user');
 
 passport.use(new FacebookStrategy({
     clientID: process.env.FACEBOOK_APP_ID,
     clientSecret: process.env.FACEBOOK_APP_SECRET,
-    callbackURL: process.env.FACEBOOK_CALLBACK
+    callbackURL: process.env.FACEBOOK_CALLBACK,
+    profileFields: ['id', 'displayName', 'first_name', 'last_name', 'photos', 'email']
 },
+function(accessToken, refreshToken, profile, cb) {
+    User.findOne({ 'facebookId': profile.id }, function(err, user) { 
+        if (err) return cb(err);
+        if (user) {
+            return cb(null, user);
+        } else {
+            var newUser = new User({
+                firstName: profile.name.givenName,
+                lastName: profile.name.familyName,
+                email: profile.emails[0].value,
+                facebookId: profile.id
+            });
+            newUser.save(function(err) {
+                if (err) return cb(err);
+                return cb(null, newUser);
+            });
+        }
+    });
+  }
 ));
+
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user) {
+        done(err, user);
+    });
+});
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,28 @@
 var express = require('express');
 var router = express.Router();
+var passport = require('passport');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function(req, res) {
+  res.render('index', { user: req.user });
+});
+
+router.get('/auth/facebook', passport.authenticate(
+  'facebook',
+  { scope: ['email']}
+));
+
+router.get('/auth/facebook/callback', passport.authenticate(
+  'facebook',
+  {
+    successRedirect : '/',
+    failureRedirect : '/'
+  }
+));
+
+router.get('/logout', function(req, res){
+  req.logout();
+  res.redirect('/');
 });
 
 module.exports = router;
