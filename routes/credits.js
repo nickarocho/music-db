ef4e32242e1a251ff1a91cdeb9f2703b6bcdfb46
Shynar Galiyeva <shynar@blacklev.com>
2018-02-08 20:02:03 -0800

    add edit/update functionality

--- routes/credits.js
+++ routes/credits.js
@@ -3,5 +3,7 @@ var router = express.Router();
 var creditCtrl = require('../controllers/credits');
 
 router.get('/', creditCtrl.index);
+router.get('/:id/edit', creditCtrl.edit);
+router.put('/:id', creditCtrl.updateCredit);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -28,6 +28,7 @@ app.set('view engine', 'ejs');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(logger('dev'));
+app.use(methodOverride('_method', { methods: ['POST', 'GET'] }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
