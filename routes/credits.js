e517817a9ac399fac537945758afd901fe493b0a
Kaylee Buscemi <kaylee.buscemi@gmail.com>
2018-02-09 11:36:11 -0800

    readme, adjust middleware

--- routes/credits.js
+++ routes/credits.js
@@ -4,8 +4,8 @@ var creditCtrl = require('../controllers/credits');
 var isLoggedIn = require('../utils/loggedIn');
 
 router.get('/', isLoggedIn, creditCtrl.index);
-router.get('/:id/edit', creditCtrl.edit);
-router.put('/:id', creditCtrl.updateCredit);
+router.get('/:id/edit', isLoggedIn, creditCtrl.edit);
+router.put('/:id', isLoggedIn, creditCtrl.updateCredit);
 
 
 module.exports = router;
