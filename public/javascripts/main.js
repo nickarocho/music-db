705ada39e2d33ebf855efd79f488e222daef9a67
Nick Arocho <nick@boldlyfine.com>
2018-02-08 17:52:58 -0800

    add modal, fix user schema names

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -21,6 +21,19 @@ function searchSpotify() {
     });
 }
 $(document).ready(function() {
+  // modal
+  $('.modal').modal({
+    dismissible: true, // Modal can be dismissed by clicking outside of the modal
+    inDuration: 300, // Transition in duration
+    outDuration: 200, // Transition out duration
+    opacity: .4, // Opacity of modal background
+    startingTop: '40%', // Starting top style attribute
+    endingTop: '30%', // Ending top style attribute
+    ready: function(modal, trigger) { // Callback for Modal open. Modal and trigger parameters available.
+      console.log(modal, trigger);
+    },
+    complete: function() {} // Callback for Modal close
+  });
   // dropdown selection
   $('select').material_select();
   // autocomplete (w/ "instruments" enum from credit model)
