3daf2b439fcbc643da1d353a7c8585f81240e43b
Kaylee Buscemi <kaylee.buscemi@gmail.com>
2018-02-08 21:00:00 -0800

    add authentication middleware

--- routes/api.js
+++ routes/api.js
@@ -2,10 +2,11 @@ var express = require('express');
 var router = express.Router();
 var apiCtrl = require('../controllers/api/credits');
 var spotifyCtrl = require('../controllers/api/spotify');
+var isLoggedIn = require('../utils/loggedIn');
 
-router.get('/api/credits', apiCtrl.getCredits);
-router.post('/api/spotify', spotifyCtrl.getSpotify);
-router.post('/api/credits', apiCtrl.createCredit);
-router.delete('/api/credits/:id', apiCtrl.deleteCredit);
+router.get('/api/credits', isLoggedIn, apiCtrl.getCredits);
+router.post('/api/spotify', isLoggedIn, spotifyCtrl.getSpotify);
+router.post('/api/credits', isLoggedIn, apiCtrl.createCredit);
+router.delete('/api/credits/:id', isLoggedIn, apiCtrl.deleteCredit);
 
 module.exports = router;
